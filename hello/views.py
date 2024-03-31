e52d5a62d01031c7b68ee4cd9cf19e6510a298e4
Frank Conejo <kuniklo.vt@gmail.com>
2024-03-31 14:22:06 +0100

    Add nav bar

--- hello/views.py
+++ hello/views.py
@@ -3,8 +3,15 @@ from django.shortcuts import render
 from django.utils.timezone import datetime
 from django.http import HttpResponse
 
+# Replace the existing home function with the one below
 def home(request):
-    return HttpResponse("Hello, Django!")
+    return render(request, "hello/home.html")
+
+def about(request):
+    return render(request, "hello/about.html")
+
+def contact(request):
+    return render(request, "hello/contact.html")
 
 # def hello_there(request, name):
 #     now = datetime.now()
